a70a48ee83171ebf7aa363eec683ab6d6bd0b6ee
anderswestberg <anders.westberg@@enera.com>
2024-09-16 13:41:38 +0200

    Added installers for ssh fs windows

--- Install-SSHFSWin.ps1
+++ Install-SSHFSWin.ps1
@@ -1,6 +1,6 @@
 # Define the URLs for downloading SSHFS-Win and WinFsp
 
-$sshfsWinUrl = "https://github.com/winfsp/sshfs-win/releases/download/v3.5.20357/sshfs-win-3.5.20357-x64.msi"
+$sshfsWinUrl = "https://github.com/enera-international/asterisk-rhel/blob/main/assets/sshfs-win-3.7.21011-x64.msi"
 $winfspUrl = "https://github.com/winfsp/winfsp/releases/download/v2.0/winfsp-2.0.23075.msi"
 
 # Define the paths where the installers will be downloaded
