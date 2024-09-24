7f899bc8a00ca6050585429751df69d95388f12f
anderswestberg <anders.westberg@@enera.com>
2024-09-24 19:00:00 +0200

    Updates

--- utilities/firewall-add-port.sh
+++ utilities/firewall-add-port.sh
@@ -13,22 +13,14 @@ PORT=$2
 PROTOCOL=$3
 
 # Check if the port is already added to the zone
-sudo firewall-cmd --zone="$ZONE" --query-port="$PORT/$PROTOCOL" > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
+if sudo firewall-cmd --zone="$ZONE" --query-port="$PORT/$PROTOCOL" > /dev/null 2>&1; then
   echo "Port $PORT/$PROTOCOL is already open in zone $ZONE."
 else
   echo "Adding port $PORT/$PROTOCOL to zone $ZONE..."
-  sudo firewall-cmd --zone="$ZONE" --add-port="$PORT/$PROTOCOL" --permanent
-
-  if [ $? -eq 0 ]; then
+  if sudo firewall-cmd --zone="$ZONE" --add-port="$PORT/$PROTOCOL" --permanent; then
     echo "Port $PORT/$PROTOCOL added successfully."
   else
     echo "Failed to add port $PORT/$PROTOCOL."
     exit 1
   fi
-
-  # Reload firewall to apply changes
-  sudo firewall-cmd --reload
-  echo "Firewall reloaded."
 fi
