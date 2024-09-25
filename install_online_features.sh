e1a137c70d3c099f3d4f9bd480ce4f588eb71345
anderswestberg <anders.westberg@@enera.com>
2024-09-25 14:25:11 +0200

    Updates

--- install_online_features.sh
+++ install_online_features.sh
@@ -146,7 +146,7 @@ install_vscode() {
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-    sudo dnf install -y code
+    sudo dnf install -y code xterm
     
     # Install VSCode extensions
     code --install-extension dbaeumer.vscode-eslint
